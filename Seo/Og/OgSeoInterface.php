2de06366f407481e08397ee85bd0d7431c51ed91
Léo <leogout@gmail.com>
2017-04-30 15:54:21 +0200

    Added OpenGraph classes

---
+++ Seo/Og/OgSeoInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Leogout\Bundle\SeoBundle\Seo\Og;
+
+use Leogout\Bundle\SeoBundle\Seo\ImageSeoInterface;
+
+/**
+ * Description of OgSeoInterface.
+ *
+ * @author: leogout
+ */
+interface OgSeoInterface extends TitleSeoInterface, DescriptionSeoInterface, ImageSeoInterface
+{
+}
