58d79ae38b3cb7a806a52255536cefab7d989be7
Léo <leogout@gmail.com>
2017-05-10 19:11:39 +0200

    Added general config and splitted og, twitter and basic

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Leogout\Bundle\SeoBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -20,42 +21,80 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('leogout_seo');
 
-        $rootNode->children()
-            ->arrayNode('title')
-                ->children()
-                    ->booleanNode('enabled')->defaultTrue()->end()
-                    ->scalarNode('content')->isRequired()->end()
-                    ->scalarNode('prefix')->defaultValue('')->end()
-                    ->scalarNode('separator')->defaultValue('-')->end()
-                ->end()
-            ->end()
-            ->arrayNode('description')
-                ->children()
-                    ->booleanNode('enabled')->defaultTrue()->end()
-                    ->scalarNode('content')->isRequired()->end()
-                ->end()
-            ->end()
-            ->arrayNode('keywords')
-                ->children()
-                    ->booleanNode('enabled')->defaultTrue()->end()
-                    ->scalarNode('content')->isRequired()->end()
-                ->end()
-            ->end()
-            ->arrayNode('robots')
-                ->children()
-                    ->booleanNode('enabled')->defaultTrue()->end()
-                    ->booleanNode('index')->defaultTrue()->end()
-                    ->booleanNode('follow')->defaultTrue()->end()
-                ->end()
-            ->end()
-            ->arrayNode('canonical')
-                ->children()
-                    ->booleanNode('enabled')->defaultTrue()->end()
-                    ->scalarNode('url')->isRequired()->end()
-                ->end()
-            ->end()
-        ;
+        $this->configureGeneralTree($rootNode);
+        $this->configureBasicTree($rootNode);
+        $this->configureOgTree($rootNode);
+        $this->configureTwitterTree($rootNode);
 
         return $treeBuilder;
     }
+
+    /**
+     * @param ArrayNodeDefinition $rootNode
+     */
+    protected function configureGeneralTree(ArrayNodeDefinition $rootNode)
+    {
+        $generalNode = $rootNode->children()->arrayNode('general');
+        $generalNode->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('title')->cannotBeEmpty()->end()
+                ->scalarNode('description')->cannotBeEmpty()->end()
+                ->scalarNode('image')->cannotBeEmpty()->end()
+            ->end();
+    }
+
+    /**
+     * @param ArrayNodeDefinition $rootNode
+     */
+    protected function configureBasicTree(ArrayNodeDefinition $rootNode)
+    {
+        $basicNode = $rootNode->children()->arrayNode('basic');
+        $basicNode->children()
+                ->scalarNode('title')->cannotBeEmpty()->end()
+                ->scalarNode('description')->cannotBeEmpty()->end()
+                ->scalarNode('keywords')->cannotBeEmpty()->end()
+                ->arrayNode('robots')
+                    ->children()
+                        ->booleanNode('index')->defaultTrue()->end()
+                        ->booleanNode('follow')->defaultTrue()->end()
+                    ->end()
+                ->end()
+                ->scalarNode('canonical')->cannotBeEmpty()->end()
+            ->end();
+    }
+
+    /**
+     * @param ArrayNodeDefinition $rootNode
+     */
+    protected function configureOgTree(ArrayNodeDefinition $rootNode)
+    {
+        $ogNode = $rootNode->children()->arrayNode('og');
+        $ogNode->children()
+                ->scalarNode('title')->cannotBeEmpty()->end()
+                ->scalarNode('description')->cannotBeEmpty()->end()
+                ->scalarNode('image')->cannotBeEmpty()->end()
+                ->scalarNode('type')->cannotBeEmpty()->end()
+                ->scalarNode('url')->cannotBeEmpty()->end()
+            ->end();
+    }
+
+    /**
+     * @param ArrayNodeDefinition $rootNode
+     */
+    protected function configureTwitterTree(ArrayNodeDefinition $rootNode)
+    {
+        $twitterNode = $rootNode->children()->arrayNode('twitter');
+        $twitterNode->children()
+                ->scalarNode('title')->cannotBeEmpty()->end()
+                ->scalarNode('description')->cannotBeEmpty()->end()
+                ->scalarNode('image')->cannotBeEmpty()->end()
+                ->scalarNode('card')->cannotBeEmpty()->end()
+                ->scalarNode('site')->cannotBeEmpty()->end()
+            ->end();
+    }
+
+
+
+
+
 }
--- DependencyInjection/LeogoutSeoExtension.php
+++ DependencyInjection/LeogoutSeoExtension.php
@@ -4,7 +4,7 @@ namespace Leogout\Bundle\SeoBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -22,11 +22,34 @@ class LeogoutSeoExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
-        $container->setParameter('leogout_seo.basic', $config);
-        $loader->load('seo/basic.xml');
+        $this->loadIfConfigured('basic', $container, $loader, $config);
+        $this->loadIfConfigured('og', $container, $loader, $config);
+        $this->loadIfConfigured('twitter', $container, $loader, $config);
 
         $loader->load('services.xml');
+    }
+
+    /**
+     * Checks if the configuration with this name isn't empty.
+     * Creates a parameter and loads a configuration file of the given configuration name.
+     *
+     * @param string $configName
+     * @param ContainerBuilder $container
+     * @param XmlFileLoader $loader
+     * @param array $config
+     */
+    private function loadIfConfigured($configName, ContainerBuilder $container, XmlFileLoader $loader, array $config)
+    {
+        if (empty($config[$configName])) {
+            return;
+        }
+        $config = array_merge($config['general'], $config[$configName]);
+        $container->setParameter(
+            sprintf('leogout_seo.%s', $configName),
+            $config
+        );
+        $loader->load(sprintf('seo/%s.xml', $configName));
     }
 }
